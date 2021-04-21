1740d4897be1af62028d9027b67628d065583eae
chaer001 <79991053+chaer001@users.noreply.github.com>
2021-04-21 08:46:20 -0700

    Cube Run Scripts
    these are the scripts that control the movements of the cube, the collision with other cubers, the map, the speed , gravity

---
+++ script/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    //after collision 
+    public PlayerMovement movement;
+
+     
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Wall" || collision.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
---
+++ script/buttonMovement.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+
+public class buttonMovement : MonoBehaviour
+{
+    // ridgid boddy
+
+    public Rigidbody rb;
+    public GameObject Player;
+
+    private float ScreenWidth;
+    // forward force speed
+    public float forwardForce = 2000f;
+
+    //side force speed
+    public float sideForce = 500f;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        ScreenWidth = Screen.width;
+        
+    }
+    void Update()
+    {
+        int i = 0;
+        while (i < Input.touchCount)
+        {
+            if (Input.GetTouch(i).position.x > Screen.width / 2)
+            {
+                runCharacter(1f);
+            }
+            if (Input.GetTouch(i).position.x < Screen.width / 2)
+            {
+                runCharacter(-1f);
+            }
+            i++;
+        }
+    }
+
+    private void runCharacter(float horizontal)
+    {
+        rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+}
